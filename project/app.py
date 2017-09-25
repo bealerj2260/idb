2fc8734fac59c5c81898612309d8f056a217b63e
Rachel Beale <bealerj@cs.utexas.edu>
2017-09-24 22:28:04 -0500

    home page now renders at '/' instead of /home

--- project/app.py
+++ project/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('home.html')
 
 
 @app.route('/players')
@@ -21,11 +21,6 @@ def about():
 @app.route('/games')
 def games():
     return render_template('games.html')
-
-
-@app.route('/home')
-def home():
-    return render_template('home.html')
 
 
 if __name__ == '__main__':
